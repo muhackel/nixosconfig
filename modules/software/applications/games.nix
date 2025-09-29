3e3287ef23b1ef7c85177f38325d9c3e93b96e07
Sebastian Rädle <muhackel@gmail.com>
2025-09-29 18:38:57 +0200

    Enable additional game support and include necessary packages in games.nix

--- modules/software/applications/games.nix
+++ modules/software/applications/games.nix
@@ -2,9 +2,17 @@
 
 lib.mkIf wantsGames {
   programs.steam.enable = true;
-  
+  programs.steam.gamescopeSession.enable = true;
+  programs.gamescope.enable = true;
+  programs.gamescope.capSysNice = true;
+  programs.gamemode.enable = true;
+  programs.gamemode.enableRenice = true;
+
   environment.systemPackages = with pkgs; [ 
     lutris
+    mangohud
+    wineWowPackages.stable #waylandFull
+    winetricks
     unigine-valley
     unigine-heaven
     unigine-tropics
