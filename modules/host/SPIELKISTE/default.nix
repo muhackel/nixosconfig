7adf2cd295e227b6a0d7445584afe061729b21ff
Sebastian Rädle <muhackel@gmail.com>
2025-10-19 12:36:30 +0200

    Add gamemode settings to HAL9000 and SPIELKISTE configurations

--- modules/host/SPIELKISTE/default.nix
+++ modules/host/SPIELKISTE/default.nix
@@ -10,4 +10,19 @@
     ../../hardware/printer/HP
     ../../hardware/printer/HP/hplj4100
   ];
+  settings = {
+    general = {
+      renice = 10;
+      #igpu_desiredgov = "performance";
+    };
+    gpu = {
+      #apply_gpu_optimisations = "accept-responsibility";
+      #gpu_device = 2;
+      #nv_powermizer_mode = 1;
+    };
+    custom = {
+      start = "${pkgs.libnotify}/bin/notify-send 'GameMode started'";
+      end = "${pkgs.libnotify}/bin/notify-send 'GameMode ended'";
+    };
+  };
 }
--- modules/software/applications/games.nix
+++ modules/software/applications/games.nix
@@ -8,21 +8,6 @@ lib.mkIf wantsGames {
   programs.gamemode = {
     enable = true;
     enableRenice = true;
-    #settings = {
-    #  general = {
-    #    renice = 10;
-    #    igpu_desiredgov = "performance";
-    #  };
-    #  gpu = {
-    #    apply_gpu_optimisations = "accept-responsibility";
-    #    gpu_device = 2;
-    #    nv_powermizer_mode = 1;
-    #  };
-    #  custom = {
-    #    start = "${pkgs.libnotify}/bin/notify-send 'GameMode started'";
-    #    end = "${pkgs.libnotify}/bin/notify-send 'GameMode ended'";
-    #  };
-    #};
   };
   environment.systemPackages = with pkgs; [
     bottles
