46f7980e36b365a2e5212e08d8b68171a7013958
Sebastian Rädle <muhackel@gmail.com>
2026-01-26 20:30:25 +0100

    Add important note for KDE Plasma keyboard settings in module/hardware/keyboard/default.nix

--- modules/hardware/keyboard/default.nix
+++ modules/hardware/keyboard/default.nix
@@ -13,4 +13,4 @@
   services.xserver.xkb.layout = "fancy_us";
   services.xserver.xkb.variant = "";
   services.xserver.xkb.options = "altgr-intl,altgr:altgr,terminate:ctrl_alt_bksp";
-}
+  ### WICHTIG!!! Unter KDE Plasma muss man in den Systemsettings unter Keyboard -> Keyboard oben rechts auf Keybindings gehen und den Key to Choose the 3rd Level auf right_Alt setzen, sonst funktioniert AltGr nicht! ###
