ea06a371c6ad18fc35973ea3520977339483baa6
Sebastian Rädle <muhackel@gmail.com>
2026-01-30 00:25:25 +0100

    refactor i18n configuration to include extra locale settings for all "formats" in german

--- modules/software/localisation/default.nix
+++ modules/software/localisation/default.nix
@@ -5,13 +5,27 @@ in
 {
   time.timeZone = "Europe/Berlin";
 
-  i18n.defaultLocale = "en_US.UTF-8";
-  i18n.supportedLocales = [
-    "en_US.UTF-8/UTF-8"
-    "de_DE.UTF-8/UTF-8"
-    "de_DE/ISO-8859-1"
-    "de_DE@euro/ISO-8859-15"
-  ];
+  i18n = {
+    defaultLocale = "en_US.UTF-8"; # Die Sprache der Menüs
+    supportedLocales = [
+      "en_US.UTF-8/UTF-8"
+      "de_DE.UTF-8/UTF-8"
+      "de_DE/ISO-8859-1"
+      "de_DE@euro/ISO-8859-15"
+    ];
+    extraLocaleSettings = {
+      LC_ADDRESS = "de_DE.UTF-8";
+      LC_IDENTIFICATION = "de_DE.UTF-8";
+      LC_MEASUREMENT = "de_DE.UTF-8"; # Metrisches System (Meter, KG)
+      LC_MONETARY = "de_DE.UTF-8";    # Währung (Euro)
+      LC_NAME = "de_DE.UTF-8";
+      LC_NUMERIC = "de_DE.UTF-8";     # Zahlenformate (Komma statt Punkt)
+      LC_PAPER = "de_DE.UTF-8";       # DIN A4 statt Letter
+      LC_TELEPHONE = "de_DE.UTF-8";
+      LC_TIME = "de_DE.UTF-8";        # Datum (TT.MM.JJJJ) und 24h-Zeit
+    };
+  };
+
   console = {
     font = "Lat2-Terminus16";
     useXkbConfig = true;
