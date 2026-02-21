4d1d3add9f142d70adae4064baa8c1e2e4ae967e
Sebastian Rädle <muhackel@gmail.com>
2026-02-21 22:31:14 +0100

    Refactor NixOS configuration to use mkHost function for host definitions

--- flake.nix
+++ flake.nix
@@ -30,119 +30,105 @@
   outputs = { self, nixpkgs, home-manager, flake-utils, lanzaboote, ... }:
     let
       lib = nixpkgs.lib;
+
+      # ── Gemeinsame Module für alle Desktop-Hosts ──
+      commonModules = [
+        ./modules/options.nix
+        ./configuration.nix
+        ./modules/user/muhackel
+        ./modules/software/fonts
+        ./modules/software/localisation
+        ./modules/software/applications
+        ./modules/software/virtualisation
+        ./modules/software/displaymanager
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.muhackel = import ./modules/user/muhackel/home.nix;
+        }
+      ];
+
+      # ── Helper: Desktop-Host erzeugen ──
+      mkHost = {
+        hostModule,
+        stateVersion ? "26.05",
+        features,
+        extraModules ? [],
+      }:
+      lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          { system.stateVersion = stateVersion;
+            local.features = features;
+          }
+          hostModule
+        ] ++ extraModules ++ commonModules;
+      };
+
     in {
       nixosConfigurations = {
-        HAL9000 = lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            ./modules/options.nix
-            { system.stateVersion = "26.05";
-              local.features = {
-                wayland = true;
-                plasma6 = true;
-                hamradio = true;
-                networking = true;
-                nfc = true;
-                ptls = true;
-                games = true;
-                docker = true;
-                winboat = true;
-                virtualbox = true;
-                libvirt = true;
-              };
-            }
-            ./configuration.nix
-            ./modules/host/HAL9000
-            ./modules/user/muhackel
-            ./modules/software/fonts
-            ./modules/software/localisation
-            ./modules/software/applications
-            ./modules/software/virtualisation
-            ./modules/software/displaymanager
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.users.muhackel = import ./modules/user/muhackel/home.nix;
-            }
-          ];
+
+        HAL9000 = mkHost {
+          hostModule = ./modules/host/HAL9000;
+          features = {
+            wayland = true;
+            plasma6 = true;
+            hamradio = true;
+            networking = true;
+            nfc = true;
+            ptls = true;
+            games = true;
+            docker = true;
+            winboat = true;
+            virtualbox = true;
+            libvirt = true;
+          };
         };
-        SPIELKISTE = lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            ./modules/options.nix
-            { system.stateVersion = "26.05";
-              local.features = {
-                wayland = true;
-                plasma6 = true;
-                hamradio = true;
-                networking = true;
-                nfc = true;
-                ptls = true;
-                games = true;
-                docker = true;
-                winboat = true;
-                virtualbox = true;
-                libvirt = true;
-              };
-              boot.loader.systemd-boot.enable = lib.mkForce false;
+
+        SPIELKISTE = mkHost {
+          hostModule = ./modules/host/SPIELKISTE;
+          features = {
+            wayland = true;
+            plasma6 = true;
+            hamradio = true;
+            networking = true;
+            nfc = true;
+            ptls = true;
+            games = true;
+            docker = true;
+            winboat = true;
+            virtualbox = true;
+            libvirt = true;
+          };
+          extraModules = [
+            lanzaboote.nixosModules.lanzaboote
+            { boot.loader.systemd-boot.enable = lib.mkForce false;
               boot.lanzaboote = {
                 enable = true;
                 pkiBundle = "/var/lib/sbctl";
               };
             }
-            lanzaboote.nixosModules.lanzaboote
-            ./configuration.nix
-            ./modules/host/SPIELKISTE
-            ./modules/user/muhackel
-            ./modules/software/fonts
-            ./modules/software/localisation
-            ./modules/software/applications
-            ./modules/software/virtualisation
-            ./modules/software/displaymanager
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.users.muhackel = import ./modules/user/muhackel/home.nix;
-            }
           ];
         };
-        BFG9000 = lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            ./modules/options.nix
-            { system.stateVersion = "26.05";
-              local.features = {
-                wayland = true;
-                plasma6 = true;
-                hamradio = true;
-                networking = true;
-                nfc = true;
-                ptls = true;
-                games = true;
-                docker = true;
-                winboat = true;
-                virtualbox = true;
-                libvirt = true;
-              };
-            }
-            ./configuration.nix
-            ./modules/host/BFG9000
-            ./modules/user/muhackel
-            ./modules/software/fonts
-            ./modules/software/localisation
-            ./modules/software/applications
-            ./modules/software/virtualisation
-            ./modules/software/displaymanager
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.users.muhackel = import ./modules/user/muhackel/home.nix;
-            }
-          ];
+
+        BFG9000 = mkHost {
+          hostModule = ./modules/host/BFG9000;
+          features = {
+            wayland = true;
+            plasma6 = true;
+            hamradio = true;
+            networking = true;
+            nfc = true;
+            ptls = true;
+            games = true;
+            docker = true;
+            winboat = true;
+            virtualbox = true;
+            libvirt = true;
+          };
         };
+
       };
     };
 }
