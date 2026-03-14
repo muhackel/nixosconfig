632894926c1871fdff3a7888882bd0911b19659b
Sebastian Rädle <muhackel@gmail.com>
2026-03-14 13:25:49 +0100

    refactor(nix.settings): Cache-Konfiguration in lib/caches.nix zentralisieren
    nix.settings in configuration.nix importiert jetzt aus lib/caches.nix
    statt die substituters/keys/trusted-users inline zu duplizieren.
    flake.nix:nixConfig bleibt inline (Flake-Schema erlaubt kein import).

--- configuration.nix
+++ configuration.nix
@@ -22,20 +22,9 @@ in
     permittedInsecurePackages = usedPermittedInsecurePackages;
   };
   nixpkgs.overlays = usedOverlays;
-  nix.settings = {
+  nix.settings = (import ./lib/caches.nix) // {
     auto-optimise-store = true;
     experimental-features = [ "nix-command" "flakes" ];
-    substituters = [
-      "https://cache.nixos.org/"
-      "https://nix-community.cachix.org"
-      "https://numtide.cachix.org"
-    ];
-    trusted-public-keys = [
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
-    ];
-    trusted-users = [ "root" "muhackel" ];
   };
   nix.gc = {
     automatic = true;
