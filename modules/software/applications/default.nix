4b7304fa4fcd125a4c7535aa7c377e14cd95c2aa
Sebastian Rädle <muhackel@gmail.com>
2026-04-17 08:37:39 +0200

    fix: flatpak-repo Service wartet auf Netzwerk
    wants/after network-online.target damit der remote-add nicht
    an fehlender Konnektivität scheitert.

--- modules/software/applications/default.nix
+++ modules/software/applications/default.nix
@@ -185,6 +185,8 @@ in
   services.flatpak.enable = true;
   systemd.services.flatpak-repo = {
     wantedBy = [ "multi-user.target" ];
+    wants = [ "network-online.target" ];
+    after = [ "network-online.target" ];
     path = [ pkgs.flatpak ];
     script = ''
       flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
