4c36350b4ff4ec071bbecde38d9e53fdcec036dd
Sebastian Rädle <muhackel@gmail.com>
2026-06-08 00:13:48 +0200

    chore: Wechsel auf nixos-unstable-small + flake.lock-Update
    unstable-small advanced früher als unstable -> häufigere Updates.
    Trade-off: weniger Binary-Cache, mehr lokale Compile-Arbeit.
    Alle Inputs aktualisiert (home-manager, claude-desktop; lanzaboote
    bleibt auf Tag v1.0.0).

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
   };
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixpkgs.url = "nixpkgs/nixos-unstable-small";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
