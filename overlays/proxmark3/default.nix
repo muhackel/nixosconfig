9850d7e31c52bae52b28b0d072d87cdea913545f
Sebastian Rädle <muhackel@gmail.com>
2026-08-21 00:03:27 +0200

    Fix: Update proxmark3 overlay to use override instead of overrideAttrs

--- overlays/proxmark3/default.nix
+++ overlays/proxmark3/default.nix
@@ -1,6 +1,6 @@
 self: super: let pkgs = super; in {
-  proxmark3 = pkgs.proxmark3.overrideAttrs (old: {
+  proxmark3 = pkgs.proxmark3.override {
     standalone = "HF_COLIN";
     withBlueshark = true;
-  });
+  };
 }
